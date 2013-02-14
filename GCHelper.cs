c0a900466b7d1820aa8fd5ba7f02a42e9e53cd5c
cerver <cervellione@cerver.net>
2013-02-14 12:53:21 -0500

    Initial Commit
    Initial Commit of GCQhull

---
+++ GCHelper.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+using System.Reflection;
+using Bentley.Geometry;
+using Bentley.GenerativeComponents;
+using Bentley.GenerativeComponents.Features.Specific;
+using Bentley.GenerativeComponents.MicroStation;
+using Bentley.Interop.MicroStationDGN;
+using Bentley.GenerativeComponents.GCScript;
+using Bentley.GenerativeComponents.GCScript.UISupport;
+using Bentley.GenerativeComponents.XceedHelpers;
+using Bentley.GenerativeComponents.Features;
+
+namespace QhullHelper
+{
+    public class QhullUtility
+    {
+        public static void OffParser(string[] inString, int dim, bool filterInf,bool isD3d, out DPoint3d[][] outPoints)
+        {
+            //parse the output
+           
+
+            //get counts
+            string[] ctLine = inString[1].Split(' ');
+            int ptNum = int.Parse(ctLine[0]);
+            int cellNum = int.Parse(ctLine[1]);
+            int faceNum = int.Parse(inString[ptNum + cellNum + 2]);
+
+            //start parsing
+            double[] ptX = new double[ptNum];
+            double[] ptY = new double[ptNum];
+            double[] ptZ = new double[ptNum];
+
+            for (int i = 0; i < ptNum; ++i)
+            {
+                
+                string[] tmpSt = inString[i + 2].Split(' ');
+                string[] wsRemovalStr = new string[3];
+                int ct = 0;
+                for (int j = 0; j < tmpSt.Length; ++j )
+                {
+                    if (tmpSt[j] != "" && ct<3)
+                    {
+                        wsRemovalStr[ct] = tmpSt[j];
+                        ct++;
+                    }
+                }
+                tmpSt = wsRemovalStr;
+                //now fill the xyz of points
+                if (dim == 2)
+                {
+                    ptX[i] = double.Parse(tmpSt[0]);
+                    ptY[i] = double.Parse(tmpSt[1]);
+                    ptZ[i] = 0;
+                }
+                else
+                {
+                    ptX[i] = double.Parse(tmpSt[0]);
+                    ptY[i] = double.Parse(tmpSt[1]);
+                    ptZ[i] = double.Parse(tmpSt[2]);
+                }
+
+            }
+
+            //get cell areas (individual polygons)
+            int[][] cellReg = new int[cellNum][];
+            int[][] faceReg = new int[faceNum][];
+            int[] fct = new int[faceNum];
+            int[] cct = new int[cellNum];
+
+            for (int i = 0; i < cellNum; ++i)
+            {
+                string[] tempSt = inString[i + 2 + ptNum].Split(' ');
+                
+                cct[i] = int.Parse(tempSt[0]);
+                cellReg[i] = new int[cct[i]];
+                
+                for (int j = 0; j < cct[i]; ++j)
+                {
+                    //Feature.Print(tempSt[j]);
+                    cellReg[i][j] = int.Parse(tempSt[j+1]);
+                }
+
+            }
+            
+            for (int i = 0; i < faceNum; ++i)
+            {
+                string[] tempSt = inString[i + 3 + ptNum + cellNum].Split(' ');
+                
+                //fct[i] = int.Parse(tempSt[0]);
+                faceReg[i] = new int[dim];
+
+                for (int j = 0; j < dim; ++j)
+                {
+                    //Feature.Print(tempSt[j]);
+                    faceReg[i][j] = int.Parse(tempSt[j]);
+                }
+
+            }
+            //if (isD3d)
+            //{
+            //    cellNum = faceNum;
+            //    cellReg = faceReg;
+            //    cct = fct;
+            //}
+            // make points
+
+            DPoint3d[][] pt3d = new DPoint3d[cellNum][];
+            
+            for (int i = 0; i < cellNum; ++i)
+            {
+                pt3d[i] = new DPoint3d[cct[i]];
+                
+                for (int j = 0; j < cct[i]; ++j)
+                {
+                    pt3d[i][j] = new DPoint3d();
+                    
+                    if (dim == 2)
+                    {
+                        pt3d[i][j].X = ptX[cellReg[i][j]];
+                        pt3d[i][j].Y = ptY[cellReg[i][j]];
+                        pt3d[i][j].Z = 0;
+                        
+                    }
+                    else
+                    {
+                        pt3d[i][j].X = ptX[cellReg[i][j]];
+                        pt3d[i][j].Y = ptY[cellReg[i][j]];
+                        pt3d[i][j].Z = ptZ[cellReg[i][j]];
+                    }
+                }
+            }
+
+            //filter infinity points for voranoi
+
+            if (filterInf)
+            {
+                
+                int ct = 0;
+                for (int i = 0; i < pt3d.Length; ++i)
+                {
+                    int isInf = 0;
+                    for (int j = 0; j < pt3d[i].Length; ++j)
+                    {
+
+                        if (pt3d[i][j].X == -10.101 && pt3d[i][j].Y == -10.101)
+                        {
+                            isInf = 1;
+
+                        }
+                    }
+                    if (isInf == 0)
+                    {
+                        ct++;
+                    }
+
+                }
+                 
+                DPoint3d[][] tempPt = new DPoint3d[ct][];
+
+                int ct2 = 0;
+                int infCheck = 0;
+                for (int i = 0; i < pt3d.Length; ++i)
+                {
+                    if (ct2 < ct)
+                    {
+                        tempPt[ct2] = new DPoint3d[pt3d[i].Length];
+
+                        infCheck = 0;
+                        for (int k = 0; k < pt3d[i].Length; ++k)
+                        {
+                            if (pt3d[i][k].X == -10.101 && pt3d[i][k].Y == -10.101)
+                            {
+                                infCheck = 1;
+                            }
+                        }
+                        if (infCheck == 0)
+                        {
+
+                            for (int j = 0; j < pt3d[i].Length; ++j)
+                            {
+                                tempPt[ct2][j] = new DPoint3d();
+                                tempPt[ct2][j] = pt3d[i][j];
+
+
+
+                            }
+                            ct2++;
+                        }
+                    }
+                }
+                pt3d = tempPt;
+            }
+            outPoints = pt3d;
+        }
+
+        public static void RunQhull(DPoint3d[] inputPoints, string qmodeString, int Dimension, ref String[] outInfo)
+        {
+            //fun starts here
+            double ptNum = inputPoints.Length;
+
+            //get qhull location 
+
+            string homeDir = Bentley.GenerativeComponents.GCScript.Configuration.UserChoices.GCWorkspaceDirectory().ToString();
+
+            string pSetCfgLoc = (homeDir + "rcQhull.cfg");
+
+            //write type
+            string qhCode = qmodeString + Dimension;
+
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string qhullLoc = Uri.UnescapeDataString(uri.Path);
+            qhullLoc = Path.GetDirectoryName(qhullLoc);
+            qhullLoc += @"\QHull\qhull.exe";
+
+
+            string arg = qmodeString + " o i s";
+
+            ProcessStartInfo psi = new ProcessStartInfo(qhullLoc);
+            psi.UseShellExecute = false;
+            psi.RedirectStandardInput = true;
+            psi.RedirectStandardOutput = true;
+            psi.CreateNoWindow = true;
+            psi.Arguments = arg;
+
+            Process strQhull;
+            strQhull = Process.Start(psi);
+
+            //process ptSet into qvoronoi format and create input file
+            StreamWriter input = strQhull.StandardInput;
+
+            //write headder
+            input.WriteLine(Dimension);
+            input.WriteLine(ptNum);
+
+            //write point location
+
+            if (Dimension == 3)
+            {
+                Random ran = new Random();
+                for (int i = 0; i < ptNum; ++i)
+                {
+                    double jig = ran.NextDouble() * 0.001;
+                    input.WriteLine((inputPoints[i].X + " " + inputPoints[i].Y + " " + (inputPoints[i].Z + jig)));
+
+                }
+
+            }
+            if (Dimension == 2)
+            {
+                for (int i = 0; i < ptNum; ++i)
+                {
+                    input.WriteLine((inputPoints[i].X + " " + inputPoints[i].Y));
+
+                }
+            }
+            input.Close();
+
+
+            StreamReader output = strQhull.StandardOutput;
+            Console.WriteLine(output);
+            string outputStr = output.ReadToEnd();
+            outInfo = outputStr.Split('\n');
+
+        }
+
+        public static DPoint3d[] orderConvexHull2d(DPoint3d[][] unOrderedPoints)
+        {
+            List<DPoint3d> orderedPoints = new List<DPoint3d>(unOrderedPoints.Length * 2);
+            
+
+            orderedPoints.AddRange(unOrderedPoints[0]);
+            DPoint3d cp;   
+
+            //for (int i = 1; i < unOrderedPoints.Length ; i++)
+            while(orderedPoints.Count != unOrderedPoints.Length)
+            {
+                cp = orderedPoints[orderedPoints.Count - 1];
+                for (int j = 1; j < unOrderedPoints.Length; j++)
+                {
+                    if (unOrderedPoints[j] != null)
+                    {
+                        if (unOrderedPoints[j][0].Distance(ref cp) < 0.001)
+                        {
+                            orderedPoints.Add(unOrderedPoints[j][1]);
+                            unOrderedPoints[j] = null;
+                            break;
+                        }
+                        if (unOrderedPoints[j][1].Distance(ref cp) < 0.001)
+                        {
+                            orderedPoints.Add(unOrderedPoints[j][0]);
+                            unOrderedPoints[j] = null;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return orderedPoints.ToArray();
+        }
+
+    }
+
+
+}
---
+++ GCQhull.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Bentley.Geometry;
+using Bentley.GenerativeComponents;
+using Bentley.GenerativeComponents.Features.Specific;
+using Bentley.GenerativeComponents.MicroStation;
+using Bentley.Interop.MicroStationDGN;
+using Bentley.GenerativeComponents.GCScript;
+using Bentley.GenerativeComponents.GCScript.Functions;
+using Bentley.GenerativeComponents.XceedHelpers;
+using QhullHelper;
+
+
+
+namespace Bentley.GenerativeComponents.Features // Must be in this namespace.
+{
+
+
+    public enum QhullCommand { Voronoi2D, Voronoi3D, ConvexHull2D, ConvexHull3D, Delaunay2D, Delaunay3D, Custom }
+    [HideInheritedTechniques]
+    public class Qhull: Polygon
+    {
+
+
+        /*------------------------------------------------------------------------------------**/
+        /// <summary>Link qhull with GC</summary>
+        /// <author>RJC</author>                                        
+        /// <date>2008</date>
+
+        /*--------------+---------------+---------------+---------------+---------------+------*/
+   
+       
+        [Technique, ByFunction]
+        public override bool ByFunction
+        (
+        FeatureUpdateContext updateContext,
+        Function Function,
+        IGCObject[] FunctionArguments
+        )
+        {
+            return base.ByFunction(updateContext, Function, FunctionArguments);
+        }
+
+        #region qhull update method
+        //  /*
+        
+
+        [DefaultTechnique]
+        public bool QhullConnect
+            (
+            FeatureUpdateContext updateContext,
+            [ Replicatable]                                 IPoint[] inputPoints,
+            [Replicatable]                                      QhullCommand qmode, 
+            [Replicatable,Optional]                             string customString,
+            [Replicatable,Optional, DefaultValue(true)]        bool createPolygon,
+            [Out]                                               ref Point[][] Points,
+            [Out]                                               ref Polygon[][] vor3dPolygon,
+            [Out]                                               ref Polygon[] polygonOut
+           
+            
+            )
+        {
+                DPoint3d[] ipToDp = Translation.ConvertToDPoint3dArray(inputPoints);
+           
+                DeleteConstituentFeatures(updateContext);
+                this.LetConstituentFeaturesBeDirectlyIndexible();
+                LetConstituentFeaturesBeDirectlyIndexible();
+                base.LetConstituentFeaturesBeDirectlyIndexible();
+
+                //parse enum
+                string qmodeString = "";
+                int Dimension = 0;
+                //fill outs
+                vor3dPolygon = null;
+                polygonOut = null;
+                
+                // var to check if delaunay 3d
+                bool isD3d = false;
+
+                bool filterInf = false;
+                if (qmode == QhullCommand.ConvexHull2D)
+                {
+                    qmodeString = "c";
+                    Dimension = 2;
+                    filterInf = false;
+                }
+                if (qmode == QhullCommand.ConvexHull3D)
+                {
+                    qmodeString = "c";
+                    Dimension = 3;
+                    filterInf = false;
+                }
+                if (qmode == QhullCommand.Delaunay2D)
+                {
+                    qmodeString = "d Qbb";
+                    Dimension = 2;
+                    filterInf = false;
+                }
+               if (qmode == QhullCommand.Delaunay3D)
+                {
+                  qmodeString = "d";
+                   Dimension = 3;
+                   isD3d = true;
+               }
+                if (qmode == QhullCommand.Voronoi2D)
+                {
+                    qmodeString = "v Qbb";
+                    Dimension = 2;
+                    filterInf = true;
+                    
+                }
+                if (qmode == QhullCommand.Voronoi3D)
+                {
+                    qmodeString = "v Qj";
+                    Dimension = 3;
+                    filterInf = true;
+
+                }
+                if (qmode == QhullCommand.Custom)
+                {
+                    string[] qms = Regex.Split(customString, ",");
+                    int cDim = int.Parse(qms[0]);
+                    qmodeString = qms[1];
+                    Dimension = cDim;
+                    filterInf = true;
+
+                }
+                
+                // run qhull
+                string[] outputStrSp = null;
+                QhullUtility.RunQhull(ipToDp, qmodeString, Dimension, ref outputStrSp);
+                
+                DPoint3d[][] dpoints; 
+                //parse the output
+                QhullUtility.OffParser(outputStrSp, Dimension, filterInf, isD3d, out dpoints);
+                //OUT POINTS
+                Point[][] p = new Point[dpoints.Length][];
+                for (int i = 0; i < dpoints.Length; i++ )
+                {
+                    p[i] = new Point[dpoints[i].Length];
+
+                    for (int j = 0; j < dpoints[i].Length; j++)
+                    {
+                        CoordinateSystem cs = this.ParentModel().BaseCoordinateSystem();
+                        p[i][j] = new Point(this);
+                        p[i][j].FromDPoint3d(updateContext, cs, dpoints[i][j]);
+                        p[i][j].SetSuccess(true);
+
+
+                    }
+
+                }
+                Points = p;
+         
+                //take care of 3d vor by finding convex hull of each region
+
+                DPoint3d[][] dpRefTemp = dpoints;
+
+                if (createPolygon)
+                {
+                    if (qmode == QhullCommand.Voronoi3D)
+                    {
+                        string[] tempOut = null;
+                        DPoint3d[][] tempDp = null;
+
+                        Polygon[][] tempPg = new Polygon[dpRefTemp.Length][];
+                        createPolygon = false;
+                        int ct = 0;
+
+                        for (int i = 0; i < dpoints.Length; i++)
+                        {
+
+                            QhullUtility.RunQhull(dpRefTemp[i], "c", 3, ref tempOut);
+                            if (tempOut.Length >= 3)
+                            {
+                                QhullUtility.OffParser(tempOut, 3, true, false, out tempDp);
+
+                                //cereate polygon
+                                tempPg[ct] = new Polygon[tempDp.Length];
+                                for (int j = 0; j < tempDp.Length; ++j)
+                                {
+                                    tempPg[ct][j] = new Polygon(this);
+                                    tempPg[ct][j].AlignOptions(this);
+                                    tempPg[ct][j].DenoteAsHavingBeenUpdated();
+                                    tempPg[ct][j].ByVerticesAsDPoint3ds(updateContext, tempDp[j], false, false, 0, null, null);
+                                    tempPg[ct][j].SetSuccess(true);
+
+                                    AddConstituentFeature(tempPg[ct][j]);
+
+                                }
+                                ct++;
+                            }
+
+
+                        }
+
+                        vor3dPolygon = tempPg;
+
+                        return true;
+                    }
+                    else if (qmode == QhullCommand.ConvexHull2D)
+                    {
+
+                        List<Polygon> tempPg = new List<Polygon>(1);
+                        Polygon pg = new Polygon(this);
+                        pg.ByVerticesAsDPoint3ds(updateContext, QhullUtility.orderConvexHull2d(dpRefTemp), false, false, 1, null, null);
+                        pg.SetSuccess(true);
+                        tempPg.Add(pg);
+                        polygonOut = tempPg.ToArray();
+                        this.AddConstituentFeatures(polygonOut);
+
+
+                        return true;
+                    }
+                    else
+                    {
+                        List<Polygon> tempPg = new List<Polygon>(dpRefTemp.Length);
+
+                        for (int j = 0; j < dpRefTemp.Length; j++)
+                        {
+                            if (dpRefTemp[j].Length > 2)
+                            {
+                                Polygon pg = new Polygon(this);
+                                pg.ByVerticesAsDPoint3ds(updateContext, dpRefTemp[j], false, false, 1, null, null);
+                                pg.SetSuccess(true);
+                                tempPg.Add(pg);
+                            }
+
+                        }
+
+                        polygonOut = tempPg.ToArray();
+                        this.AddConstituentFeatures(polygonOut);
+
+                        return true;
+                    }
+                }
+
+                return false;
+
+        }
+        // */
+        #endregion
+
+      
+    
+    } // class
+
+} // namespace
